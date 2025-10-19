94e6b7de8249ac9ab63c956cf154af3cfea07787
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-10-19 08:39:28 +0600

    v .01

--- Meme Generator/src/components/Main.jsx
+++ Meme Generator/src/components/Main.jsx
@@ -1,8 +1,9 @@
 /** @format */
-import React from "react";
+
+import { useState, useEffect } from "react";
 
 export default function Main() {
-	const [meme, setMeme] = React.useState({
+	const [meme, setMeme] = useState({
 		topText: "One does not simply",
 		bottomText: "Walk into Mordor",
 		imageUrl: "http://i.imgflip.com/1bij.jpg",
@@ -15,9 +16,17 @@ export default function Main() {
 			.then((data) => setAllMemes(data.data.memes));
 	}, []);
 
+	function getMemeImage() {
+		const randomNumber = Math.floor(Math.random() * allMemes.length);
+		const newMemeUrl = allMemes[randomNumber].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			imageUrl: newMemeUrl,
+		}));
+	}
+
 	function handleChange(event) {
 		const { value, name } = event.currentTarget;
-
 		setMeme((prevMeme) => ({
 			...prevMeme,
 			[name]: value,
@@ -48,7 +57,9 @@ export default function Main() {
 						value={meme.bottomText}
 					/>
 				</label>
-				<button>Get a new meme image 🖼</button>
+				<button onClick={getMemeImage}>
+					Get a new meme image 🖼
+				</button>
 			</div>
 			<div className="meme">
 				<img src={meme.imageUrl} />
