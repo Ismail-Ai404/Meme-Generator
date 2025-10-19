f67c65c75630de6dc89c2e5bfcaf8275d444f29b
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-10-19 07:12:54 +0600

    first commit

---
+++ Meme Generator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+export default function Header() {
+	return (
+		<header className="header">
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZfajCxCvw9jaeE7_R1Ujt4ofVbTlqfOvog&s" />
+			<h1>Meme Generator</h1>
+		</header>
+	);
+}
