f9c082e921a989ed1bb32b6ca891cb36bf102856
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-10-19 08:24:43 +0600

    add useEffect to fetch data

--- Meme Generator/src/components/Main.jsx
+++ Meme Generator/src/components/Main.jsx
@@ -7,6 +7,13 @@ export default function Main() {
 		bottomText: "Walk into Mordor",
 		imageUrl: "http://i.imgflip.com/1bij.jpg",
 	});
+	const [allMemes, setAllMemes] = useState([]);
+
+	useEffect(() => {
+		fetch("https://api.imgflip.com/get_memes")
+			.then((res) => res.json())
+			.then((data) => setAllMemes(data.data.memes));
+	}, []);
 
 	function handleChange(event) {
 		const { value, name } = event.currentTarget;
